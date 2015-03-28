5d7d527b073cf268ba80a5b562942d4c4e236c29
joelle3 <joelle3@brandeis.edu>
2015-03-28 11:57:42 -0400

    Joelle

--- main.rb
+++ main.rb
@@ -1,9 +1,8 @@
 puts "Hello World"
 puts "Ruby is really fun"
 puts "what is up"
-<<<<<<< HEAD
+
 
 puts "Joelle did this"
-=======
+
 puts "Michael"
->>>>>>> 0205a170f01f5154fd308a1bbe44541095be4bf0
